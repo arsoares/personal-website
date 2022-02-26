eed79399b35f6a8e969ec0a7399099c913a9a523
Austin Soares <arsoares@me.com>
2022-02-26 18:06:56 -0500

    Fixed menu

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,5 @@
 $('#menu-toggle').click(function() {
    $('#menu-overlay').toggleClass('open');
+   $('#menu-toggle').toggleClass('close-button');
+   $('#nav-main').toggleClass('frozen-nav');
 });
